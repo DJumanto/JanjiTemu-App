2e67f7153734440138f4620823b9d247280a8792
Arfi23 <arfifaizilraushani@gmail.com>
2023-10-17 23:21:03 +0700

    Membuat form pada halaman add_page

--- resources/views/app_users/addpage.blade.php
+++ resources/views/app_users/addpage.blade.php
@@ -3,12 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Add User</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Add a user</h1>
-    <form method="post" action="">
-        <input type="name" name="name">
+    <h1 class="container ms-2">Add a user</h1>
+
+    <form class="container ms-3">
+        <div class="mb-3">
+            <label for="nama" class="form-label">Nama</label>
+            <input type="text" class="form-control" id="nama">
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email">
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control" id="password">
+        </div>
+        <div class="mb-3">
+            <label for="status">Status</label>
+                <select name="status" id="status">
+                    <option value="member">Member</option>
+                    <option value="admin">Admin</option>
+                </select>
+        </div>
+        <button type="add" class="btn btn-primary">Add User</button>
     </form>
 </body>
 </html>

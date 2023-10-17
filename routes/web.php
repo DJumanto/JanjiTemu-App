06cac765179b9ae305e48a2348b9b6e216ca0c2c
Arfi23 <arfifaizilraushani@gmail.com>
2023-10-17 16:55:49 +0700

    Membuat route untuk app_user

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/app_user', [App_User_Controller::class, 'index'])->name('app_user.index');

7253a015d53dc69616914e85fe41348ea2de5ed1
Arfi23 <arfifaizilraushani@gmail.com>
2023-10-17 20:03:59 +0700

    Menambahkan fungsi add pada App_User_Controller

--- app/Http/Controllers/App_User_Controller.php
+++ app/Http/Controllers/App_User_Controller.php
@@ -7,7 +7,11 @@ use Illuminate\Http\Request;
 class App_User_Controller extends Controller
 {
     public function index(){
-        
+        return view('app_users.index');
+    }
+
+    public function add(){
+        return view('app_users.addpage');
     }
 
 }

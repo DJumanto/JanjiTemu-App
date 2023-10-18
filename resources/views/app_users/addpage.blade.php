04583ca592fda96eec5827b5ec774ebc8a6902f2
Arfi23 <arfifaizilraushani@gmail.com>
2023-10-18 09:50:54 +0700

    Memperbaiki form penambahan data user, yang sebelumnya tidak muncul di dd

--- resources/views/app_users/addpage.blade.php
+++ resources/views/app_users/addpage.blade.php
@@ -13,36 +13,17 @@
         <!-- class="container ms-3"  -->
         @csrf
         @method('post')
-        <!-- <div class="mb-3">
+        <div class="mb-3">
             <label for="nama" class="form-label">Nama</label>
-            <input type="text" class="form-control" id="nama">
+            <input type="text" class="form-control" name="nama">
         </div>
         <div class="mb-3">
             <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email">
+            <input type="email" class="form-control" name="email">
         </div>
         <div class="mb-3">
             <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="password">
-        </div>
-        <div class="mb-3">
-            <label for="status">Status</label>
-                <select name="status" id="status">
-                    <option value="member">Member</option>
-                    <option value="admin">Admin</option>
-                </select>
-        </div> -->
-        <div>
-            <label for="nama" class="form-label">Nama</label>
-            <input type="text" class="form-control" id="nama">
-        </div>
-        <div>
-            <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email">
-        </div>
-        <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="password">
+            <input type="password" class="form-control" name="password">
         </div>
         <div class="mb-3">
             <label for="status">Status</label>
@@ -51,9 +32,7 @@
                     <option value="admin">Admin</option>
                 </select>
         </div>
-        <div>
-            <input type="submit" value="Add User">
-        </div>
+        <button id="button1" type="submit" class="btn btn-primary mt-2">Submit</button>
     </form>
 </body>
 </html>

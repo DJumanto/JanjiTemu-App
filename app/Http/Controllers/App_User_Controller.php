6343e7933a71022670689776e8bfac03073d201e
Arfi23 <arfifaizilraushani@gmail.com>
2023-10-18 10:57:54 +0700

    Membuat fungsi add

--- app/Http/Controllers/App_User_Controller.php
+++ app/Http/Controllers/App_User_Controller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\App_User;
 
 class App_User_Controller extends Controller
 {
@@ -15,6 +16,23 @@ class App_User_Controller extends Controller
     }
 
     public function add(Request $request){
-        dd($request);
+        // dd($request);
+        $messages = [
+            'nama.required' => 'Kolom nama wajib diisi.',
+            'email.required' => 'Kolom email wajib diisi.',
+            'email.email' => 'Format email tidak valid.',
+            'password.required' => 'Kolom password wajib diisi.',
+            'password.min' => 'Password harus terdiri dari minimal :min karakter.',
+        ];
+
+        $data = $request->validate([
+            'nama' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8'
+        ], $messages);
+
+        $newApp_User = App_User::create($data);
+
+        return redirect(route('app_user.index'));
     }
 }

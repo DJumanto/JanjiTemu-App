7870b4573570ce238903ae80c2d9113c7261a7d2
Alfa Fakhrur <100863813+DJumanto@users.noreply.github.com>
2023-11-22 13:57:33 +0700

    fix: app structure

--- app/Http/Controllers/GetEventController.php
+++ app/Http/Controllers/GetEventController.php
@@ -4,13 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Http\Repositories\SqlEvent;
+use App\Http\Services\EventServices\GetSomeEventService;
 
 class GetEventController extends Controller
 {
-    public function GetSomeEventLists(SqlEvent $sqlEvent){
-        $results = $sqlEvent->GetSomeEventLists(3);
+    public function GetSomeEventLists(GetSomeEventService $getSomeEventService){
+        $results = $getSomeEventService->execute(3);
         return view('index', ['results' => $results]);
-
     }
 }
--- app/Http/Repositories/SqlEvent.php
+++ app/Http/Repositories/SqlEvent.php
@@ -3,14 +3,9 @@
 namespace App\Http\Repositories;
 use Illuminate\Support\Facades\DB;
 class SqlEvent{
-    public function GetSomeEventLists($count){
+    public function getSomeEventLists($count){
         $results = DB::table('events')->select('e_name','e_place', 'e_date', 'e_image', 'g_name', 'e_date')
         ->join('groups', 'events.Group_g_uuid', '=', 'groups.g_id')->limit($count)->latest('events.created_at')->get();
-        foreach($results as $result){
-            $splitted = explode(" ", $result->e_date);
-            $result->e_date = date('l, d F Y', strtotime($splitted[0]));
-            $result->e_time = date('h:i A', strtotime($splitted[1]));
-        }
         return $results;
     }
 }

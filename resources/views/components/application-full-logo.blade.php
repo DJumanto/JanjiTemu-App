26955dd790f9bf9a098e38a7c77f8bbc2049ee57
Arfi <108170236+Arfi23@users.noreply.github.com>
2023-11-30 18:43:16 +0700

    feat: refactor login and register page
    * fix: delete language dropdown and modify login button
    
    * fix: add styling
    
    * fix: add footer for landing page
    
    * feat: add friend illustration images
    
    * fix: vite needs
    
    * feat: change logo trial
    
    * fix: add logo
    
    * feat: add logo images
    
    * fix: move to postgre
    
    * fix: delete an icon
    
    * fix: fix dashboard logo sizing trial

--- resources/views/components/application-full-logo.blade.php
+++ resources/views/components/application-full-logo.blade.php
@@ -1,3 +1,3 @@
 <svg viewBox="0 0 316 316" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-    <image xlink:href="img/janjitemu-logo-big-no.png" width="100%" height="100%" />
+    <image xlink:href="img/janjitemu-full-logo.png" width="100%" height="100%" />
 </svg>

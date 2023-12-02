47ed7e6db4f78a657b472234df3a2a6d73804c06
Arfi23 <arfifaizilraushani@gmail.com>
2023-12-02 23:31:21 +0700

    fix: edit navbar and add bootstrap in app

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,21 +1,25 @@
-<nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+<nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700 p-2">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
+
+            <!-- Logo -->
             <div class="flex">
-                <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-full-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <!-- <x-application-full-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" /> -->
+                        <img src="{{ asset('img/janjitemu-full-logo-new.png') }}" alt="" class="h-16">
                     </a>
                 </div>
+            </div>
 
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
+            <!-- Search Event and Group -->
+            <div class="shrink-0 flex items-center">
+                <form class="d-flex flex-row " role="search">
+                    <input class="form-control me-2 rounded" type="search" placeholder="Cari event" aria-label="Search" style="border-color: rgba(0, 0, 0, 0.25);">
+                    <input class="form-control me-2 rounded" type="search" placeholder="Kota terdekat" aria-label="Search" style="border-color: rgba(0, 0, 0, 0.25);">
+                    <button class="btn btn-outline-primary" type="submit"><i class="bi bi-search"></i></button>
+                </form>
             </div>
 
             <!-- Settings Dropdown -->

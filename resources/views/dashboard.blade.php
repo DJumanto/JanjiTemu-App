55d10c569cdbb46af526330d85c8e07829c57e3e
Arfi23 <arfifaizilraushani@gmail.com>
2023-12-04 02:03:37 +0700

    fix: finish editing the dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,75 +3,6 @@
         <h1 class="display-6"><b>Selamat datang,</b> <b><strong> [User]</strong></b></h1>
     </x-slot>
 
-    <!-- <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div> -->
-    <!-- Part Upcoming Event & More -->
-    <div class="row">
-        <div class="col-md-9 ms-5 mt-5">
-            <h2 class="h2">Event yang akan datang</h2 </div>
-        </div>
-        <div class="col-md ms-auto mt-5">
-            <a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="#" style="color:#7879DF; font-weight:600;">
-                <br>
-                <h6>Lihat selengkapnya</h6>
-            </a>
-        </div>
-    </div>
-
-    <!-- Part Event Card -->
-    <div class="row ms-4 mt-4">
-        <div class="col-md-3">
-            <div class="card" style="width: 18rem;">
-                <img src="/img/event1.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title"><a class="link-dark link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="#">Business Insight 2023</a></h5>
-                    <p class="card-text">Hosted by : Proxsis Indonesia</p>
-                    <p><i class="bi bi-calendar4-event"></i> Senin, 27 NOV 2023 - 9:00 WIB</p>
-                    <h6><i class="bi bi-ticket-perforated"></i> FREE</h6>
-                    <div class="float-end">
-                        <a href="#" class="btn" style="text-decoration: underline; text-decoration-color: #7879DF; text-decoration-thickness: 1.5px; text-underline-offset: 10px; font-weight:700; color:#7879DF">Daftar sekarang</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card" style="width: 18rem;">
-                <img src="/img/event2.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Kidscares Charity Night</h5>
-                    <p class="card-text">Hosted by : Zanes Kidscare</p>
-                    <p><i class="bi bi-calendar4-event"></i> Minggu, 20 OCT 2023 - 11:30 WIB</p>
-                    <h6><i class="bi bi-ticket-perforated"></i> FREE</h6>
-                    <div class="float-end">
-                        <a href="#" class="btn" style="text-decoration: underline; text-decoration-color: #7879DF; text-decoration-thickness: 1.5px; text-underline-offset: 10px; font-weight:700; color:#7879DF">Daftar sekarang</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card" style="width: 18rem;">
-                <img src="/img/event3.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Brooklyn NYC Dribble Meetup</h5>
-                    <p class="card-text">Hosted by : Brooklyn HQ</p>
-                    <p><i class="bi bi-calendar4-event"></i> Kamis, 30 JUN 2023 - 19:00 WIB</p>
-                    <h6><i class="bi bi-ticket-perforated"></i> FREE</h6>
-                    <div class="float-end">
-                        <a href="#" class="btn" style="text-decoration: underline; text-decoration-color: #7879DF; text-decoration-thickness: 1.5px; text-underline-offset: 10px; font-weight:700; color:#7879DF">Daftar sekarang</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <br><br>
-
     <!-- Part Your Event -->
     <div class="row">
         <div class="col-md-9 ms-5 mt-5">
@@ -144,4 +75,132 @@
         </div>
     </div>
 
+
+    <br><br><br><br>
+    <!-- Part Your Group -->
+    <div class="row">
+        <div class="col-md-9 ms-5 mt-5">
+            <h2 class="h4">Group Anda</h2 </div>
+        </div>
+        <div class="col-md ms-auto mt-5">
+            <a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="#" style="color:#7879DF; font-weight:600;">
+                <h6>Lihat semua group</h6>
+            </a>
+        </div>
+    </div>
+
+    <br>
+    <!-- Part Purple Box -->
+    <div class="row rounded p-3" style="background-color:#7879DF;">
+        <div class="col-md ms-2">
+            <div class="row row-gap-3 justify-content-center">
+                <div class="col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <div class="position-relative">
+                            <img src="/img/event4.jpg" class="card-img-top rounded-circle img-thumbnail position-absolute start-0 top-0 ms-2 mt-2" alt="..." style="width: 70px; height: 70px; object-fit: cover;">
+                        </div>
+                        <div class="card-body">
+                            <div class="ms-5">
+                                <h3 class="card-title ms-4"><strong>Autumn Party Group</strong></h3>
+                            </div>
+                            <div class="float-end">
+                                <a href="#" class="btn" style="text-decoration: underline; text-decoration-color: #7879DF; text-decoration-thickness: 1.5px; text-underline-offset: 10px; font-weight:700; color:#7879DF">Lihat grup</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <div class="position-relative">
+                            <img src="/img/event5.jpeg" class="card-img-top rounded-circle img-thumbnail position-absolute start-0 top-0 ms-2 mt-2" alt="..." style="width: 70px; height: 70px; object-fit: cover;">
+                        </div>
+                        <div class="card-body">
+                            <div class="ms-5">
+                                <h3 class="card-title ms-4"><strong>Kadin Expo 2023 Group</strong></h3>
+                            </div>
+                            <div class="float-end">
+                                <a href="#" class="btn" style="text-decoration: underline; text-decoration-color: #7879DF; text-decoration-thickness: 1.5px; text-underline-offset: 10px; font-weight:700; color:#7879DF">Lihat grup</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <div class="position-relative">
+                            <img src="/img/event6.jpg" class="card-img-top rounded-circle img-thumbnail position-absolute start-0 top-0 ms-2 mt-2" alt="..." style="width: 70px; height: 70px; object-fit: cover;">
+                        </div>
+                        <div class="card-body">
+                            <div class="ms-5">
+                                <h3 class="card-title ms-4"><strong>Control Group</strong></h3>
+                            </div>
+                            <div class="float-end">
+                                <a href="#" class="btn" style="text-decoration: underline; text-decoration-color: #7879DF; text-decoration-thickness: 1.5px; text-underline-offset: 10px; font-weight:700; color:#7879DF">Lihat grup</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <div class="position-relative">
+                            <img src="/img/event2.jpg" class="card-img-top rounded-circle img-thumbnail position-absolute start-0 top-0 ms-2 mt-2" alt="..." style="width: 70px; height: 70px; object-fit: cover;">
+                        </div>
+                        <div class="card-body">
+                            <div class="ms-5">
+                                <h3 class="card-title ms-4"><strong>Kids Care Group</strong></h3>
+                            </div>
+                            <div class="float-end">
+                                <a href="#" class="btn" style="text-decoration: underline; text-decoration-color: #7879DF; text-decoration-thickness: 1.5px; text-underline-offset: 10px; font-weight:700; color:#7879DF">Lihat grup</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <div class="position-relative">
+                            <img src="/img/event3.jpg" class="card-img-top rounded-circle img-thumbnail position-absolute start-0 top-0 ms-2 mt-2" alt="..." style="width: 70px; height: 70px; object-fit: cover;">
+                        </div>
+                        <div class="card-body">
+                            <div class="ms-5">
+                                <h3 class="card-title ms-4"><strong>Brooklyn Group</strong></h3>
+                            </div>
+                            <div class="float-end">
+                                <a href="#" class="btn" style="text-decoration: underline; text-decoration-color: #7879DF; text-decoration-thickness: 1.5px; text-underline-offset: 10px; font-weight:700; color:#7879DF">Lihat grup</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <div class="position-relative">
+                            <img src="/img/event1.png" class="card-img-top rounded-circle img-thumbnail position-absolute start-0 top-0 ms-2 mt-2" alt="..." style="width: 70px; height: 70px; object-fit: cover;">
+                        </div>
+                        <div class="card-body">
+                            <div class="ms-5">
+                                <h3 class="card-title ms-4"><strong>Insight Group</strong></h3>
+                            </div>
+                            <div class="float-end">
+                                <a href="#" class="btn" style="text-decoration: underline; text-decoration-color: #7879DF; text-decoration-thickness: 1.5px; text-underline-offset: 10px; font-weight:700; color:#7879DF">Lihat grup</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <div class="position-relative">
+                            <img src="/img/event7.jpg" class="card-img-top rounded-circle img-thumbnail position-absolute start-0 top-0 ms-2 mt-2" alt="..." style="width: 70px; height: 70px; object-fit: cover;">
+                        </div>
+                        <div class="card-body">
+                            <div class="ms-5">
+                                <h3 class="card-title ms-4"><strong>Off-Road Group</strong></h3>
+                            </div>
+                            <div class="float-end">
+                                <a href="#" class="btn" style="text-decoration: underline; text-decoration-color: #7879DF; text-decoration-thickness: 1.5px; text-underline-offset: 10px; font-weight:700; color:#7879DF">Lihat grup</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
 </x-app-layout>

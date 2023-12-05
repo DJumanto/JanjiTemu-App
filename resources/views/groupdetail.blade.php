fa7ca5f9f74ee0b5d4084b0ac70ed58b37e22809
Arfi23 <arfifaizilraushani@gmail.com>
2023-12-05 11:01:14 +0700

    feat: finish the group detail page

--- resources/views/groupdetail.blade.php
+++ resources/views/groupdetail.blade.php
@@ -67,11 +67,11 @@
 
             </div>
         </div>
-        <br>
+        <br><br><br>
         <div id="members">
             <h4 class="h4">Anggota Grup</h4>
             <div class="scroll" style="height: 300px; overflow: scroll;">
-                <table class="table">
+                <table class="table table-hover">
                     <thead>
                         <tr>
                             <th scope="col">#</th>
@@ -133,11 +133,74 @@
                 </table>
             </div>
         </div>
-        <br><br><br><br><br><br><br><br><br>
+        <br><br><br>
         <div id="events">
-            <h4 class="h4">Ini list event grup ini</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit blanditiis assumenda nulla magni sapiente cum aliquam nesciunt minus at dolorem autem illo, sunt voluptatum id, minima molestiae, atque beatae ut!</p>
+            <h4 class="h4">Event grup ini</h4>
+            <!-- Part Event of The Group Card -->
+            <div class="grid gap-0 row-gap-3" id="group-event-card">
+                <div class="card position-relative" style="width: 100%;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="/img/event4.jpg" class="card-img-top img-fluid" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title"><a class="link-dark link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="#">Autumn Party</a></h5>
+                                <p class="card-text">Hosted by: Weather Indonesia</p>
+                                <p><i class="bi bi-calendar4-event"></i> Minggu, 15 OCT 2023 - 9:00 WIB</p>
+                                <h6><i class="bi bi-ticket-perforated"></i> FREE</h6>
+                                <div class="position-absolute bottom-0 end-0 mb-3 me-3">
+                                    <button href="#" class="btn btn-primary" style="color:white;">Edit Event</button>
+                                    <button href="#" class="btn btn-danger" style="color:white">Delete Event</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- test 3 -->
+                <div class="card position-relative" style="width: 100%;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="/img/event5.jpeg" class="card-img-top img-fluid" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title"><a class="link-dark link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="#">Kadin Banten Expo 2022</a></h5>
+                                <p class="card-text">Hosted by: Pemprov Banten</p>
+                                <p><i class="bi bi-calendar4-event"></i> Minggu, 19 DEC 2023 - 11:30 WIB</p>
+                                <h6><i class="bi bi-ticket-perforated"></i> FREE</h6>
+                                <div class="position-absolute bottom-0 end-0 mb-3 me-3">
+                                    <button href="#" class="btn btn-primary" style="color:white;">Edit Event</button>
+                                    <button href="#" class="btn btn-danger" style="color:white">Delete Event</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end test -->
+                <div class="card position-relative" style="width: 100%;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="/img/event6.jpg" class="card-img-top img-fluid" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title"><a class="link-dark link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="#">Control Premiere</a></h5>
+                                <p class="card-text">Hosted by: Remedy Entertainment</p>
+                                <p><i class="bi bi-calendar4-event"></i> Kamis, 27 AUG 2023 - 19:00 WIB</p>
+                                <h6><i class="bi bi-ticket-perforated"></i> FREE</h6>
+                                <div class="position-absolute bottom-0 end-0 mb-3 me-3">
+                                    <button href="#" class="btn btn-primary" style="color:white;">Edit Event</button>
+                                    <button href="#" class="btn btn-danger" style="color:white">Delete Event</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
+        <br><br><br><br>
     </div>
 
 

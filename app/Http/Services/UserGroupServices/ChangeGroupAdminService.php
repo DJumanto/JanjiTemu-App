e53c4b659a45ba41e44835d98cb58c63e890b861
Alfa Fakhrur <100863813+DJumanto@users.noreply.github.com>
2023-12-06 01:08:05 +0700

    feat: add mailer (#38)

--- app/Http/Services/UserGroupServices/ChangeGroupAdminService.php
+++ app/Http/Services/UserGroupServices/ChangeGroupAdminService.php
@@ -2,13 +2,29 @@
 
 namespace App\Http\Services\UserGroupServices;
 use App\Http\Repositories\SqlUserGroup;
+use App\Http\Repositories\SqlUser;
+use App\Http\Repositories\SqlGroupRole;
+use App\Mail\PrivilegeNotifMail;
+use Illuminate\Support\Facades\Mail;
 class ChangeGroupAdminService{
     private SqlUserGroup $sqlUserGroup;
-    public function __construct(SqlUserGroup $sqlUserGroup){
+    private SqlUser $sqlUser;
+    private SqlGroupRole $sqlGroupRole;
+
+    public function __construct(SqlUserGroup $sqlUserGroup, SqlUser $sqlUser, SqlGroupRole $sqlGroupRole){
         $this->sqlUserGroup = $sqlUserGroup;
+        $this->sqlUser = $sqlUser;
+        $this->sqlGroupRole = $sqlGroupRole;
     }
     public function execute(string $groupId, int $userId, int $adminId){
         $this->sqlUserGroup->setPrivilege($groupId, $userId, 2);
         $this->sqlUserGroup->setPrivilege($groupId, $adminId, 1);
+        $user = $this->sqlUser->getUsernameByID($userId);
+        $user = $user->first_name . " " . $user->last_name;
+        $email = $this->sqlUser->getEmailByID($userId);
+        $master = $this->sqlUserGroup->getUserInGroupByRoleId(1, $groupId)[0];
+        $master = $master->first_name . " " . $master->last_name;
+        $privilege = 'master';
+        Mail::to($email)->send(new PrivilegeNotifMail($user, $privilege, $master));
     }
 }

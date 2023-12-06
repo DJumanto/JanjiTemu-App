420cd4e7a896af2839225a094403d1cfefaa8d8d
Arfi23 <arfifaizilraushani@gmail.com>
2023-12-06 08:05:23 +0700

    fix: finish designing the event detail page including attendees

--- resources/views/eventdetail.blade.php
+++ resources/views/eventdetail.blade.php
@@ -2,19 +2,77 @@
     <x-slot name="header">
         <h2 class="h2"><b>[Nama Event]</b></h2>
         <p class="text-secondary">[jumlah] peserta terdaftar</p>
-        <p class="text-secondary">Diselenggarakan oleh [Nama Grup Penyelenggara]</p>
+        <p class="text-secondary">Diselenggarakan oleh <a class="link-underline link-underline-opacity-0" href="#">[Nama Grup Penyelenggara]</a></p>
     </x-slot>
 
     <div class="row">
-        <div class="col">
+        <div class="col-7">
             <figure class="figure">
                 <img src="/img/event3.jpg" class="figure-img img-fluid rounded" alt="..." style="max-width: 550px;">
             </figure>
         </div>
         <div class="col">
-            <h5 class="h5"><strong>[Nama Grup]</strong></h5>
-            <p class="h6 mt-3 text-body-tertiary"><i class="bi bi-people"></i> 100 orang</p>
+            <h5 class="h5"><strong><a class="link-underline link-underline-opacity-0" href="">[Nama Grup Penyelenggara]</a></strong></h5>
+            <p class="h6 mt-3 text-body-tertiary"><i class="bi bi-people"></i> [jumlah anggota grup] orang</p>
             <p class="h6 mt-3 text-body-tertiary"><i class="bi bi-person-check"></i> Dibuat oleh <b>Tester Bro</b></p>
+            <br><br>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Pelaksanaan Event</h5>
+                    <p class="card-text"><i class="bi bi-calendar4-event"></i> [Tanggal Pelaksanaan Event]</p>
+                    <p class="card-text"><i class="bi bi-geo-alt"></i>[Tempat Pelaksanaan Event]</p>
+                </div>
+            </div>
+            <!-- button to write a comment -->
+            <div class="d-grid d-md-flex mt-5 justify-content-start">
+                <button class="btn btn-info btn comment-btn me-5" href="#" role="button">Beri Komentar <i class="bi bi-chat-left-dots"></i></button>
+            </div>
         </div>
+    </div>
+    <div class="row">
+        <div class="col-8">
+            <h4 class="h4">Deskripsi Event</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nesciunt impedit numquam in ex tempore excepturi ipsam nemo, iure sit perspiciatis nisi amet cum aut sapiente delectus aliquam? Veniam dicta distinctio, saepe doloribus incidunt facere ea perferendis! Quos ducimus tempore consequuntur animi repudiandae eius. Fugiat magni cum corrupti aut ratione repudiandae impedit perferendis modi ducimus porro quae quos dolorum dolor assumenda illum, explicabo possimus, quidem sint consequuntur, distinctio fuga molestiae. Dicta voluptates soluta vel ut commodi dolore omnis, magnam officia aliquam itaque voluptatem consequatur iusto laborum dignissimos deserunt! Optio, in blanditiis. Vero sunt excepturi neque inventore eos ipsum amet maiores.</p>
+        </div>
+    </div>
+    <br><br>
+    <div class="row">
+        <div class="col">
+            <h4 class="h4">Peserta</h4>
+        </div>
+        <div class="col">
+            <a href="#">Lihat semua</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-6">
+            <div class="row rounded p-2" style="background-color:#7879DF;">
+                <div class="card profile-card mb-3" style="width: 11rem; height:15rem; margin-right: 10px;">
+                    <img src="/img/friends-res-no.png" class="card-img-top profile-img" alt="Profile Picture">
+                    <div class="card-body">
+                        <h5 class="card-title">[Nama Akun]</h5>
+                        <p class="text-body-secondary"><em>[role]</em></p>
+                    </div>
+                </div>
+                <div class="card profile-card mb-3" style="width: 11rem; height:15rem; margin-right: 10px;">
+                    <img src="/img/friends-joyce-no.png" class="card-img-top profile-img" alt="Profile Picture">
+                    <div class="card-body">
+                        <h5 class="card-title">[Nama Akun]</h5>
+                        <p class="text-body-secondary"><em>[role]</em></p>
+                    </div>
+                </div>
+                <div class="card profile-card mb-3" style="width: 11rem; height:15rem; margin-right: 10px;">
+                    <img src="/img/JanjiTemu-logo-big-no.png" class="card-img-top profile-img" alt="Profile Picture">
+                    <div class="card-body">
+                        <h5 class="card-title">[Nama Akun]</h5>
+                        <p class="text-body-secondary"><em>[role]</em></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- button to register to the event -->
+    <div class="d-grid d-md-flex mt-3 justify-content-end">
+        <button class="btn btn-primary btn-lg custom-btn me-5" href="#" role="button">Gabung Event <i class="bi bi-plus-circle"></i></button>
     </div>
 </x-app-layout>
